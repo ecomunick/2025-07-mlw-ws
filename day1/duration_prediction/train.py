ae86b355b73c364e3983a47ca7baff5fae34d86f
MCabral <75102223+ecomunick@users.noreply.github.com>
2025-07-03 11:30:22 +0000

    updating files and folders

--- day1/duration_prediction/train.py
+++ day1/duration_prediction/train.py
@@ -11,33 +11,72 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import make_pipeline
 import argparse
+from loguru import logger
 
-def read_dataframe(filename):
-    df = pd.read_parquet(filename)
+def read_dataframe(filename: str):
+    """
+    Reads a Parquet file into a pandas DataFrame, calculates trip duration in minutes, 
+    filters out trips outside the 1-60 minute range, and converts specified columns to string.
 
-    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
-    df.duration = df.duration.dt.total_seconds() / 60
+    Parameters:
+    ----------
+    filename : str
+        Path to the Parquet file containing trip data.
 
-    df = df[(df.duration >= 1) & (df.duration <= 60)]
+    Returns:
+    -------
+    pd.DataFrame
+        A cleaned DataFrame with an added 'duration' column (in minutes), 
+        filtered for valid trip durations, and with categorical location IDs 
+        converted to string type.
+    """
+    logger.info(f"loading file: {filename}")
+    try:
+        df = pd.read_parquet(filename)
 
-    categorical = ['PULocationID', 'DOLocationID']
-    df[categorical] = df[categorical].astype(str)
-    
-    return df
+        df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+        df.duration = df.duration.dt.total_seconds() / 60
+
+        df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+        categorical = ['PULocationID', 'DOLocationID']
+        df[categorical] = df[categorical].astype(str)
+        logger.info(f"{filename} had {len(df)} rows")
+        return df
+    except Exception as e:
+        logger.error(f"error reading: {filename}")
+        logger.error(e)
+        raise
 
 
 def train(train_date: date, val_date: date, out_path: str):
+    """
+    Trains a linear regression model to predict trip duration based on NYC green taxi trip data.
+
+    Downloads training and validation datasets based on input dates, processes them,
+    fits a model using DictVectorizer and LinearRegression, evaluates it using RMSE, 
+    and saves the trained pipeline to a file.
+
+    Parameters
+    ----------
+    train_date : date
+        The date representing the training dataset's month and year.
+    val_date : date
+        The date representing the validation dataset's month and year.
+    out_path : str
+        Path where the trained model pipeline will be saved (as a pickle file).
+
+    Returns
+    -------
+    None
+        The function saves the trained model pipeline to the specified path and prints RMSE.
+    """
     base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet'
     train_url = base_url.format(year=train_date.year, month=train_date.month)
     val_url = base_url.format(year=val_date.year, month=val_date.month)
     
-    print(f"{train_url=}")
-    print(f"{val_url=}")
-
     df_train = read_dataframe(train_url)
     df_val = read_dataframe(val_url)
-
-    print(f"train_data length: {len(df_train)}, val_data length: {len(df_val)}")
 
     categorical = ['PULocationID', 'DOLocationID']
     numerical = ['trip_distance']
@@ -57,23 +96,10 @@ def train(train_date: date, val_date: date, out_path: str):
     y_pred = pipeline.predict(val_dicts)
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    print(f"{mse=}")
+    logger.info(f"{mse=}")
 
+    logger.info(f"writing model into {out_path}")
     with open(out_path, 'wb') as f_out:
         pickle.dump(pipeline, f_out)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train a model based on specified dates and save it to a given path")
-    parser.add_argument("--train-date", required=True, help="train month in the YYYY-MM format")
-    parser.add_argument("--val-date", required=True, help="validation month in the YYYY-MM format")
-    parser.add_argument("--model-save-path", required=True, help="Path where the trained model should be saved")
     
-    args = parser.parse_args()
-    train_year, train_month = args.train_date.split("-")
-    val_year, val_month = args.val_date.split("-")
-    
-    train_date = date(int(train_year), int(train_month), 1)
-    val_date = date(int(val_year), int(val_month), 1)
-    out_path = args.model_save_path
-
-    train(train_date, val_date, out_path)
+    return mse
--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from day1!")
+    print("Hello from day-1!")
 
 
 if __name__ == "__main__":
